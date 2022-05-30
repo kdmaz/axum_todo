190d808c66e8f1afd5b40a543c35212cef1e7a70
kyle <kyle.mazeika@gmail.com>
2022-05-30 10:34:18 -0400

    get session cookie

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use async_redis_session::RedisSessionStore;
 use axum::{Extension, Server};
 use tower::ServiceBuilder;
 mod pool;
@@ -6,8 +7,11 @@ mod routes;
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let pool = pool::get_pool().await;
+    let store = RedisSessionStore::new("redis://127.0.0.1:6379")?;
 
-    let middleware = ServiceBuilder::new().layer(Extension(pool));
+    let middleware = ServiceBuilder::new()
+        .layer(Extension(pool))
+        .layer(Extension(store));
 
     let app = routes::get_router().layer(middleware);
 
--- src/routes/login.rs
+++ src/routes/login.rs
@@ -1,15 +1,24 @@
-use axum::{http::StatusCode, Extension, Json};
+use async_redis_session::RedisSessionStore;
+use async_session::{Session, SessionStore};
+use axum::{
+    http::{header::SET_COOKIE, StatusCode},
+    response::{AppendHeaders, IntoResponse},
+    Extension, Json,
+};
+use cookie::{time::Duration, Cookie, CookieJar, Key};
 use secrecy::Secret;
 use sqlx::{query, PgPool};
 use todo::{
     auth::{validate_user, Credentials},
     user::User,
 };
+use uuid::Uuid;
 
 pub async fn post(
     Json(login): Json<Credentials>,
     Extension(pool): Extension<PgPool>,
-) -> StatusCode {
+    Extension(store): Extension<RedisSessionStore>,
+) -> impl IntoResponse {
     let user = query!(
         r#"
 			SELECT id, password_hash
@@ -27,13 +36,44 @@ pub async fn post(
     });
 
     match validate_user(user, login.password).await {
-        Ok(user_id) => {
-            dbg!(user_id);
-            // TODO - session
-            // store.create_session_with_user_id(user_id)
-
-            StatusCode::OK
-        }
-        Err(_) => StatusCode::UNAUTHORIZED,
+        Ok(user_id) => match get_session_cookie(user_id, store).await {
+            Ok(cookie) => {
+                let headers = AppendHeaders([(SET_COOKIE, cookie)]);
+                dbg!(headers);
+                Ok(StatusCode::OK)
+            }
+            Err(_) => Err(StatusCode::INTERNAL_SERVER_ERROR),
+        },
+        Err(_) => Err(StatusCode::UNAUTHORIZED),
     }
+}
+
+async fn get_session_cookie(
+    user_id: Uuid,
+    store: RedisSessionStore,
+) -> Result<Cookie<'static>, anyhow::Error> {
+    let mut session = Session::new();
+    session.expire_in(std::time::Duration::from_secs(4 * 60 * 60));
+    session.insert(user_id.to_string().as_str(), user_id)?;
+
+    let cookie_value = store
+        .store_session(session)
+        .await?
+        .ok_or(anyhow::anyhow!("No cookie value found"))?;
+
+    let cookie = Cookie::build("id", cookie_value)
+        .http_only(true)
+        .max_age(Duration::hours(4))
+        .finish();
+    let mut jar = CookieJar::new();
+    let key =
+        Key::from("super-long-and-secret-random-key-needed-to-verify-message-integrity".as_bytes());
+    jar.private_mut(&key).add(cookie);
+    let cookie = jar
+        .delta()
+        .next()
+        .ok_or(anyhow::anyhow!("No cookie found"))?
+        .clone();
+
+    Ok(cookie)
 }
